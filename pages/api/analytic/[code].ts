58807f3232c61f09ad03c8c5942f6207c2c468c5
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-06-30 11:07:38 +0000

    Added Analytic code

---
+++ pages/api/analytic/[code].ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/libs/index";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  if (method !== "GET") {
+    return res.status(400).json({
+      message: "Only GET request are allowed!",
+    });
+  }
+
+  const { code } = req.query;
+
+  if (typeof code == "string") {
+    const analytic = await prisma.urlAnalytic.findFirst({
+      where: {
+        url: {
+          alias: code,
+        },
+      },
+      include: {
+        url: true,
+      },
+    });
+
+    if (!analytic) {
+      return res.status(400).json({
+        statusCode: 400,
+        error: {
+          message: "Analytic not found!",
+        },
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      statusCode: 200,
+      error: null,
+      data: {
+        clicked: analytic.clicked,
+        url: {
+          originalUrl: analytic.url.originalUrl,
+          shortUrl: analytic.url.shortUrl,
+          code: analytic.url.alias,
+        },
+      },
+    });
+  }
+}
