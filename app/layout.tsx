9f0caf0ccdba82df21a3bf36777a28bd12bcb7a9
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-06-30 13:16:42 +0000

    Save local changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,21 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+     
+      <head />
+      <body>
+        <div
+          className={`relative h-full w-full flex justify-center items-center ${inter.className}`}
+        >
+          <div className="flex flex-col shadow-2xl p-4 w-[798px] mx-auto rounded-lg">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
